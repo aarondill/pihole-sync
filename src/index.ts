233f1121da59749a43e70ea4b8d9219beb38348e
Aaron Dill <aaronsacks2006@gmail.com>
2026-02-28 11:43:49 -0600

    fix: stop when fail

--- src/index.ts
+++ src/index.ts
@@ -24,14 +24,14 @@ const config: JSONConfig = JSON.parse(await readFile(configFile, "utf-8"));
 // Pushes current config to Pi-hole
 // NOTE: does *not* remove any domains or lists that are not in the config
 async function push(s: Session) {
-  let ok = true;
-
   for (const type of objectKeys(config.lists)) {
     for (const list of config.lists[type]) {
       const response = await postList(s, { type, address: list });
       if (response.ok) console.log(`Updated ${type} list ${list}`);
-      else console.error(`Failed to add list ${list}:`, response.data);
-      ok &&= response.ok;
+      else {
+        console.error(`Failed to add list ${list}:`, response.data);
+        throw new Error("Failed to update");
+      }
     }
   }
 
@@ -39,14 +39,14 @@ async function push(s: Session) {
     for (const domain of config.domains[type]) {
       const response = await postDomain(s, { type, ...domain });
       if (response.ok) console.log(`Updated ${type} domain ${domain.domain}`);
-      else
+      else {
         console.error(`Failed to add domain ${domain.domain}:`, response.data);
-      ok &&= response.ok;
+        throw new Error("Failed to update");
+      }
     }
   }
 
-  if (!ok) throw new Error("Failed to update");
-  else console.log("Update done!");
+  console.log("Update done!");
 
   console.log("Updating gravity.db");
   const res = await updateGravity(s);
