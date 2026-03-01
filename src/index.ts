46764289b66a5118be7f1a0b226b28722ee3cdf3
Aaron Dill <aaronsacks2006@gmail.com>
2026-02-28 22:03:21 -0600

    fix: index to api class

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import { readFile } from "node:fs/promises";
 import { Writable } from "node:stream";
 import { objectKeys } from "tsafe/objectKeys";
 import { z } from "zod";
-import { Api, USER_AGENT } from "./api/index.ts";
-import { Domain, List, SID } from "./api/types.ts";
+import { Pihole } from "./api/index.ts";
+import { Domain, List } from "./api/types.ts";
 
 const DomainConfig = Domain.omit({ type: true });
 type DomainConfig = z.infer<typeof DomainConfig>;
@@ -29,12 +29,12 @@ const config = Config.decode(await readFile(configFile, "utf-8"));
 
 // Pushes current config to Pi-hole
 // NOTE: does *not* remove any domains or lists that are not in the config
-async function push(s: SID) {
+async function push(api: Pihole) {
   // Chore: fetch and diff first.
   // Don't run gravity if nothing has changed.
   for (const type of objectKeys(config.lists)) {
     for (const list of config.lists[type]) {
-      const response = await Api.Lists.POST(s, { type, address: list });
+      const response = await api.Lists.POST({ type, address: list });
       if (response.ok) console.log(`Updated ${type} list ${list}`);
       else {
         console.error(`Failed to add list ${list}:`, response.data);
@@ -45,7 +45,7 @@ async function push(s: SID) {
 
   for (const type of objectKeys(config.domains)) {
     for (const domain of config.domains[type]) {
-      const response = await Api.Domains.POST(s, { type, ...domain });
+      const response = await api.Domains.POST({ type, ...domain });
       if (response.ok) console.log(`Updated ${type} domain ${domain.domain}`);
       else {
         console.error(`Failed to add domain ${domain.domain}:`, response.data);
@@ -57,7 +57,7 @@ async function push(s: SID) {
   console.log("Update done!");
 
   console.log("Updating gravity.db");
-  const res = await Api.Actions.Gravity.POST(s);
+  const res = await api.Actions.Gravity();
   if (!res.ok) console.log("Gravity Failed: ", res.data);
   else
     await res.data.pipeTo(Writable.toWeb(process.stdout), {
@@ -66,24 +66,11 @@ async function push(s: SID) {
     });
   console.log("Done!");
 }
-async function cleanupSessions(s: SID) {
-  const sessions = await Api.Auth.Sessions.GET(s);
-  if (!sessions.ok) throw new Error("Failed to get sessions");
-  const remove = sessions.data
-    .filter(s => !s.current_session && s.user_agent === USER_AGENT)
-    .map(({ id }) => id);
-  console.log("Removing old sessions...");
-  await Promise.all(remove.map(id => Api.Auth.Sessions.DELETE(s, id)));
-  console.log("Done!");
-}
 
+export const API_URL = new URL(process.env.PIHOLE_API || "http://pi.hole/api");
 const password = process.env.PIHOLE_PASSWORD ?? "";
-const res = await Api.Auth.POST({ password });
-if (!res.ok) throw new Error("Failed to create session");
-const session = res.data;
-
-await cleanupSessions(session);
-await push(session);
+await using pihole = await new Pihole(API_URL).login(password);
+await push(pihole);
 // Keep this process alive forever
-setInterval(() => void 0, 2 ** 30);
+// setInterval(() => void 0, 2 ** 30);
 // TODO: pull on an interval
